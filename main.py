25f3875eb7dc3b85c533b10850df4df743dd370a
AyrielNoriega <anoriega2310@gmail.com>
2024-11-01 19:32:54 -0500

    add ErrorHandler

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 from config.database import engine, Base
 from publications.router import publication_router
 from users.router import user_router
+from middlewares.error_handler import ErrorHandler
 
 app = FastAPI()
 app.title = "Insta Publications API"
 app.version = "0.0.1"
+app.add_middleware(ErrorHandler)
 
 Base.metadata.create_all(bind=engine)
 
