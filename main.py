08ee90edf2dff8d424acd5ca1256bd7cda8510ea
AyrielNoriega <anoriega2310@gmail.com>
2024-11-03 17:39:41 -0500

    Refactor user and authentication routes

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from datetime import timedelta
+from fastapi import Depends, FastAPI
 
 from core import database
 from publications.router import publication_router
-from users.router import user_router
+from users.router.users import user_router
+from users.router.auth import auth_router
 from middlewares.error_handler import ErrorHandler
-from users.schemas.token import Token
-from users.schemas.user import User
-from users.service import auth
-from core.security import create_access_token
 from core.config import get_settings
 
 settings = get_settings()
@@ -27,27 +21,4 @@ database.Base.metadata.create_all(bind=database.engine)
 
 app.include_router(publication_router)
 app.include_router(user_router)
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(auth.get_current_active_user)]):
-    return current_user
-
-
-@app.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-) -> Token:
-    user = auth.authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
+app.include_router(auth_router)
---
+++ users/router/auth.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from datetime import timedelta
+
+from users.schemas.token import Token
+from users.service import auth
+from users.schemas.user import User
+from core.security import create_access_token
+from core.config import get_settings
+
+settings = get_settings()
+
+
+auth_router = APIRouter()
+auth_router.prefix = "/auth"
+auth_router.tags = ["auth"]
+
+
+# @auth_router.get("/users/me")
+# async def read_users_me(current_user: Annotated[User, Depends(auth.get_current_active_user)]):
+#     return current_user
+
+
+@auth_router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = auth.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
